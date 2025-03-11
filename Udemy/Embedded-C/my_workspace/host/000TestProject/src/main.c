d71a01b7bd1ec55d735c1034eec09ea01cd24a0f
Siddhesh Naik <e010naik@gmail.com>
2025-03-11 12:37:57 +0530

    added hello world sample for windows host

---
+++ Udemy/Embedded-C/my_workspace/host/000TestProject/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 11, 2025
+ *      Author: siddh
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+	printf("Hello this is Siddhesh Naik.!!\r\nWe are gonna crack STM32 soon.!!\r\n");
+}
